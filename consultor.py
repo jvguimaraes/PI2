c810651b876904dfe350b474e592e8d64d6d5eee
jvguimaraes <guimaraes.joao.v@gmail.com>
2020-11-26 13:43:47 -0300

    Added search to consultor

--- consultor.py
+++ consultor.py
@@ -11,3 +11,16 @@ def cadastro_consultor():
     dados['lista_de_especialidades'] = input('Lista de Especialidades: ')
     consultores.append(dados)
     # print(dados)
+
+
+def pesquisa_consultor():
+    pesquisa = input("digite a sua pesquisa: ")
+    lista_pesquisa = []
+
+    tamanho = len(consultores)
+
+    for i in range(tamanho):
+        if pesquisa in (consultores[i]['nome']):
+            lista_pesquisa.append(consultores[i]['consultor'])
+
+    print(f"foram encontrados os seguintes consultores: \n {lista_pesquisa}")
